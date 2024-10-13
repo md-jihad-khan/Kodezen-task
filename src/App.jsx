8a0d83dd4a65b077d9579a9c2786a55acd66f0c7
md-jihad-khan <mdjihadkhan9867@gmail.com>
2024-10-14 03:23:27 +0600

    created the tab components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import DesignSystem from "./pages/designSystem/DesignSystem";
+import Integration from "./pages/integration/Integration";
+import General from "./pages/General/General";
+import Tabs from "./components/tabs/Tabs";
 
 function App() {
+  const [activeTab, setActiveTab] = useState(1);
+
+  const tabComponents = [<General />, <DesignSystem />, <Integration />];
+
   return (
     <>
       <Navbar />
+
+      <h5 className="">Setting</h5>
+      <div className="container">
+        <div className="left-container">
+          <Tabs
+            tabs={["General", "DesignSystem", "Integration"]}
+            onTabChange={setActiveTab}
+          />
+        </div>
+
+        <div className="right-container">{tabComponents[activeTab]}</div>
+      </div>
     </>
   );
 }
---
+++ src/components/tabs/Tabs.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./Tabs.css";
+
+const Tabs = ({ tabs, onTabChange }) => {
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+    onTabChange(index);
+  };
+
+  return (
+    <div className="kzui-tabs">
+      {tabs.map((tab, index) => (
+        <button
+          key={index}
+          className={`kzui-tabs__tab ${
+            activeTab === index ? "kzui-tabs__tab--active" : ""
+          }`}
+          onClick={() => handleTabClick(index)}
+        >
+          {tab}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Tabs;
